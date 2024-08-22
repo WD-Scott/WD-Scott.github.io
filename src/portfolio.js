bd701f1f9d32fba4c0d37dd3e1f699670212cfed
Wyatt Scott <122546418+WD-Scott@users.noreply.github.com>
2024-08-22 18:47:00 -0400

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -19,6 +19,13 @@ import { FaAws } from "react-icons/fa";
 import emoji from "react-easy-emoji";
 import splashAnimation from "./assets/lottie/splashAnimation"; // Rename to your file name for custom animation
 
+// Splash Screen
+const splashScreen = {
+  enabled: true, // set false to disable splash screen
+  animation: splashAnimation,
+  duration: 2000 // Set animation duration as per your animation
+};
+
 // Summary And Greeting Section
 const illustration = {
   animated: false // Set to false to use static SVG
