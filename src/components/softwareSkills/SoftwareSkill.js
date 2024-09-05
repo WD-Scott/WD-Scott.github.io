7be55a6d700690446addbe44c493a69f247ea1e6
Wyatt Scott <122546418+WD-Scott@users.noreply.github.com>
2024-09-05 06:37:31 -0400

    Add files via upload

--- src/components/softwareSkills/SoftwareSkill.js
+++ src/components/softwareSkills/SoftwareSkill.js
@@ -1,24 +1,23 @@
 import React from "react";
 import "./SoftwareSkill.scss";
-import { skillsSection } from "../../portfolio";
-import { SiTensorflow } from "react-icons/si";
-import { FaPython } from "react-icons/fa";
-import { FaRProject } from "react-icons/fa";
-import { FaLinux } from "react-icons/fa";
-import { SiScikitlearn } from "react-icons/si";
-import { SiPandas } from "react-icons/si";
-import { SiNumpy } from "react-icons/si";
-import { SiPytorch } from "react-icons/si";
-import { SiKeras } from "react-icons/si";
-import { FaGithub } from "react-icons/fa";
-import { FaHtml5 } from "react-icons/fa";
-import { SiSqlite } from "react-icons/si";
-import { BiLogoPostgresql } from "react-icons/bi";
-import { GrMysql } from "react-icons/gr";
-import { TbBrandSnowflake } from "react-icons/tb";
-import { TbBrandDatabricks } from "react-icons/tb";
-import { FaAws } from "react-icons/fa";
-
+import {skillsSection} from "../../portfolio";
+import {SiTensorflow} from "react-icons/si";
+import {FaPython} from "react-icons/fa";
+import {FaRProject} from "react-icons/fa";
+import {FaLinux} from "react-icons/fa";
+import {SiScikitlearn} from "react-icons/si";
+import {SiPandas} from "react-icons/si";
+import {SiNumpy} from "react-icons/si";
+import {SiPytorch} from "react-icons/si";
+import {SiKeras} from "react-icons/si";
+import {FaGithub} from "react-icons/fa";
+import {FaHtml5} from "react-icons/fa";
+import {SiSqlite} from "react-icons/si";
+import {BiLogoPostgresql} from "react-icons/bi";
+import {GrMysql} from "react-icons/gr";
+import {TbBrandSnowflake} from "react-icons/tb";
+import {TbBrandDatabricks} from "react-icons/tb";
+import {FaAws} from "react-icons/fa";
 
 export default function SoftwareSkill() {
   return (
